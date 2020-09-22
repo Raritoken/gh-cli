0d83b478592b8a244115943b2166512d206d3c13
Raritoken <0956540@hr.nl>
2020-09-22 21:59:03 +0200

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ function myFunction() {
     if (x.style.display === "block") {
       x.style.display = "none";
       window.alert("YOU DARED TO PRESS THE BUTTON AGAIN!?\nHERESY!\nHERESY DARE I SAY!😡🤬", window);
-      window.open("http://twitch.tv/raritoken", window);
+      window.open("https://setuplog.io/setup/MGNArBX2/raritokens-desktop-build", window);
     } 
     else {
       x.style.display = "block";
@@ -30,5 +30,23 @@ function Discord() {
   var x = document.getElementById("discord");
   if (x.onclick) {
     window.open("https://discord.gg/t7MpXYD", window);
+  }
+}
+function YouTube() {
+  var x = document.getElementById("YTRari");
+    if (x.onclick) {
+      window.open("http://youtube.com/raritoken", window);
+    }
+  }
+function Twitch() {
+  var x = document.getElementById("twitch");
+  if (x.onclick) {
+    window.open("https://twitch.tv/raritoken/about", window);
+  }
+}
+function Top() {
+  var x = document.getElementById("ScrlTop");
+  if (x.onclick) {
+    window.scrollTo(1,1);
   }
 }
